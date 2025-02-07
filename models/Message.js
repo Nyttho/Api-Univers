719d7ff3a9d0a36be2c27ac4784e2e1a3b2ae31b
Nyttho <anthony.dms@outlook.com>
2025-02-07 10:01:52 +0100

    adding getLastMessage method to Message model

--- models/Message.js
+++ models/Message.js
@@ -3,18 +3,35 @@ import pool from "../lib/db.js";
 
 class Message extends BaseModel {
   constructor() {
-    super("messages"); // Nom de la table
+    super("messages");
   }
 
   async getAllMessages(convId) {
     const query = `SELECT * FROM ${this.tableName} WHERE chatbox_id = $1`;
 
     try {
-      const result = await pool.query(query, [convId]); // ✅ Ajout de `await`
+      const result = await pool.query(query, [convId]);
       return result.rows;
     } catch (error) {
       console.error("Erreur SQL:", error);
       throw new Error("Impossible de récupérer les messages");
+    }
+  }
+
+  async getLastMessage(convId) {
+    const query = `
+      SELECT * FROM ${this.tableName} 
+      WHERE chatbox_id = $1 
+      ORDER BY created_at DESC 
+      LIMIT 1
+    `;
+
+    try {
+      const result = await pool.query(query, [convId]);
+      return result.rows[0] || null; // Retourne null si aucun message trouvé
+    } catch (error) {
+      console.error("Erreur SQL:", error);
+      throw new Error("Impossible de récupérer le dernier message");
     }
   }
 }
